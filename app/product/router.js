e3b8fcdae9f81a174884d37eba4028c6a3254d75
Aji Maulana <mancode700@gmail.com>
2021-12-08 14:02:39 +0700

    menambah depedencies, menambah penangan input file gambar

--- app/product/router.js
+++ app/product/router.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const os = require('os');
+
 const productController = require('./controller');
 
 const router = require('express').Router();
 const multer = require('multer');
 
-router.post('/products', multer().none(), productController.store);
+router.post('/products', multer({dest: os.tmpdir()}).single('url_gambar'), productController.store);
+
 
 module.exports = router;
