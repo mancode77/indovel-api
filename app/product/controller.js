79b13c773ed0d612230bbfcf419ddde245087eae
Aji Maulana <mancode700@gmail.com>
2021-12-08 14:00:19 +0700

    menabah core package, menambah fitur upload file gambar, validasi input

--- app/product/controller.js
+++ app/product/controller.js
@@ -1,13 +1,15 @@
 'use strict'
 
+const fs = require('fs');
+const path = require('path');
 const { Query } = require('./model');
 const { connection } = require('./../../database');
+const config = require('./../config');
 
 async function store(req, res, next) {
     try{
         // * tangkap request dari user
         let payload = req.body;
-
         // * tampung data payload
         const dataPayload = [];
         
@@ -16,33 +18,134 @@ async function store(req, res, next) {
             dataPayload.push(payload[key]);
         }
 
-        // * instance object query sql
+         // * instance object query sql
         let query = new Query();
 
-        // * mulai transaction
-        await connection.beginTransaction();
-        
-          // * insert data 
-        await connection.query(
-            query.sqlInsert(), 
-            [[dataPayload]], 
-            function(err, rows) {
-                // * tangani gagal query
-                if(err) {
-                    // * rollback
-                    return connection.rollback(function(err) {
-                        console.error(`Gagal melakukan query: ${err}`);
-                        return;
-                    });
-                }
+          // * mulai transaction
+        await connection.beginTransaction(function(err) {
+            if(err) {
+                console.error(`Gagal melakukan transaction: ${err}`);
+                return;
+            }
         });
 
-        // * commit
-        await connection.commit();
+        // * cek apakah melakukan request file
+        if(req.file) {
+            let tmp_path = req.file.path;
+            let originalExt =
+                req.file.originalname.split(".")[
+                req.file.originalname.split(".").length - 1
+                ];
+            let filename = `${req.file.filename}.${originalExt}`;
+            let target_path = path.resolve(
+                config.rootPath,
+                `public/upload/${filename}`
+            );
 
-        // * return data json 
-        return res.json();
+            // * baca file yang masih di lokasi sementara
+            let src = fs.createReadStream(tmp_path);
+            // *  pindahkan file ke lokasi permanen
+            let dest = fs.createWriteStream(target_path);
+            // *  mulai pindahkan file dari `src` ke `dest`
+            src.pipe(dest);
+
+            src.on("end", async () => {
+                let product = [...dataPayload, filename];
+                //! buat function untuk query, karena dipakai berulang
+                await connection.query(
+                    query.sqlInsert(), 
+                    [[product]], 
+                    async function(err, rows) {
+                        // * tangani gagal query
+                        if(err) {
+                            // * rollback
+                            await connection.rollback(function(err) {
+                                console.error(`Gagal melakukan query: ${err}`);
+                            });
+
+                            // //! penanganan error ( sementara )
+                            return res.json({
+                                error: 1,
+                                message: err.sqlMessage,
+                            });
+                        }
+                });
+
+                // ! select data untuk mengetahui data yang di inputkan user ( sementara )
+                await connection.query(
+                    query.sqlSelect("SELECT * FROM produk ORDER BY id DESC LIMIT 1"),  
+                    async function(err, rows) {
+                        // * tangani gagal query
+                        if(err) {
+                            // * rollback
+                            await connection.rollback(function(err) {
+                                console.error(`Gagal melakukan query: ${err}`);
+                            });
+                        } 
+                        return res.json(rows);
+                });
+            });
+
+            src.on("error", async (err) => {
+                next(err);
+            });
+        } else {
+            // * insert data 
+            await connection.query(
+                query.sqlInsert(), 
+                [[dataPayload]], 
+                async function(err, rows) {
+                    // * tangani gagal query
+                    if(err) {
+                        // * rollback
+                        await connection.rollback(function(err) {
+                            console.error(`Gagal melakukan query: ${err}`);
+                        });
+
+                        // //! penanganan error ( sementara )
+                        return res.json({
+                            error: 1,
+                            message: err.sqlMessage,
+                        });
+                    }
+            });
+
+            // ! select data untuk mengetahui data yang di inputkan user ( sementara )
+            await connection.query(
+                query.sqlSelect("SELECT * FROM produk ORDER BY id DESC LIMIT 1"),  
+                async function(err, rows) {
+                    // * tangani gagal query
+                    if(err) {
+                        // * rollback
+                        await connection.rollback(function(err) {
+                            console.error(`Gagal melakukan query: ${err}`);
+                        });
+                    } 
+                    return res.json(rows);
+            });
+        }
+
+        // * commit
+        await connection.commit(function(err) {
+            if(err) {
+                console.error(`Gagal melakuka commit: ${err}`);
+                return;
+            }
+        });
     }catch (err) {
+        /**
+        * ! Penangan belum bisa digunakan
+        * ! Membutuhan decorator!!...
+         */
+        // * cek tipe error
+        if(err && err.name === 'ValidationError'){
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+
         // * penanganan error oleh express
         next(err);
     }
