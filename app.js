48e7bc7e57c735c838af1831b3488089a26c8b9c
Aji Maulana <mancode700@gmail.com>
2021-12-08 13:57:07 +0700

    menginstall beberapa package dan menambahkan core package

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 'use strict'
 
+const path = require('path');
 const productRouter = require('./app/product/router');
-
+const createError = require('http-errors');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // * router api
 app.use('/api', productRouter);
