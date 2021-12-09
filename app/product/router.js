62b53d7c2eb472e154d7a1d365feaaeea6b33d02
Aji Maulana <mancode700@gmail.com>
2021-12-09 10:53:48 +0700

    menambah endpoint query read dan update data

--- app/product/router.js
+++ app/product/router.js
@@ -7,6 +7,8 @@ const productController = require('./controller');
 const router = require('express').Router();
 const multer = require('multer');
 
+router.get('/products', productController.index);
 router.post('/products', multer({dest: os.tmpdir()}).single('image_url'), productController.store);
+router.put('/products/:id', multer({dest: os.tmpdir()}).single('image_url'), productController.update);
 
 module.exports = router;
