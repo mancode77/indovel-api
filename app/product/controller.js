294d27c0f1ff61d078a8e67f77558b25f5fba3ae
Aji Maulana <mancode700@gmail.com>
2021-12-09 10:52:55 +0700

    menambah function read dan update data

--- app/product/controller.js
+++ app/product/controller.js
@@ -6,6 +6,9 @@ const { Query } = require('./model');
 const { connection } = require('./../../database');
 const config = require('./../config');
 
+// * instance object query sql
+let query = new Query();
+
 async function store(req, res, next) {
     try{
         // * user request data 
@@ -17,9 +20,6 @@ async function store(req, res, next) {
         for(let key in payload) {
             dataPayload.push(payload[key]);
         }
-
-         // * instance object query sql
-        let query = new Query();
 
           // * start transaction
         await connection.beginTransaction(function(err) {
@@ -51,7 +51,15 @@ async function store(req, res, next) {
 
             src.on("end", async () => {
                 let product = [...dataPayload, filename];
+
+                /**
+                 * ! check connection query 
+                 * ! jika true, return data
+                 * ! jik false, return error
+                 */
+
                 //! make a function for the query, because it is used over and over 
+                //  * insert data
                 await connection.query(
                     query.sqlInsert(), 
                     [[product]], 
@@ -151,4 +159,222 @@ async function store(req, res, next) {
     }
 }
 
-module.exports = { store };
+async function index(req, res, next) {
+    try {
+
+        let { limit = 10, skip = 0 } = req.query;
+
+          // * start transaction
+        await connection.beginTransaction(function(err) {
+            if(err) {
+                console.error(`Failed to make a transaction : ${err}`);
+                return;
+            }
+        });
+
+         // ! select data to find out the data entered by the user (temporarily) 
+        await connection.query(
+            //! Binding limit and skip
+            query.sqlSelect(`SELECT * FROM products ORDER BY id DESC LIMIT ${Number(skip)}, ${Number(limit)}`),  
+            async function(err, rows) {
+                // * handle failed query 
+                if(err) {
+                    // * rollback
+                    await connection.rollback(function(err) {
+                        console.error(`Query failed: ${err}`);
+                    });
+                } 
+                
+                return res.json(rows);
+        });
+
+         // * commit
+        await connection.commit(function(err) {
+            if(err) {
+                console.error(`Failed to commit : ${err}`);
+                return;
+            }
+        });
+    }catch(err) {
+         // * error handling by express 
+        next(err);
+    }
+}
+
+// ! not tested yet 
+async function update(req, res, next) {
+    try{
+        // * user request data 
+        let payload = req.body;
+        // * tampung data payload
+        const dataPayload = [];
+
+        // * Capture user id 
+        dataPayload.push(req.params.id);
+        
+        // * fetch attributes from payload 
+        for(let key in payload) {
+            dataPayload.push(payload[key]);
+        }
+
+          // * start transaction
+        await connection.beginTransaction(function(err) {
+            if(err) {
+                console.error(`Failed to make a transaction : ${err}`);
+                return;
+            }
+        });
+
+        // * check whether to make a file request 
+        if(req.file) {
+            let tmp_path = req.file.path;
+            let originalExt =
+                req.file.originalname.split(".")[
+                req.file.originalname.split(".").length - 1
+                ];
+            let filename = `${req.file.filename}.${originalExt}`;
+            let target_path = path.resolve(
+                config.rootPath,
+                `public/upload/${filename}`
+            );
+
+            // * read files that are still in a temporary location 
+            let src = fs.createReadStream(tmp_path);
+            // *  move files to a permanent location 
+            let dest = fs.createWriteStream(target_path);
+            // *  start moving files from `src` to `dest` 
+            src.pipe(dest);
+
+            src.on("end", async () => {
+                let product = [...dataPayload, filename];
+
+                // * get the absolute path to the image of the product to be updated 
+                let currentImage = `${config.rootPath}/public/upload/${product[4]}`;
+
+                // * check if the absolute path does exist in the file system
+                if(fs.existsSync(currentImage)){
+                    // * if there is delete it from the file system 
+                    fs.unlinkSync(currentImage)
+                }
+
+                /**
+                 * ! cek conection query
+                 * ! if true, return data
+                 * ! if false, return error
+                 */
+
+                //! make a function for the query, because it is used over and over 
+
+                // * update data
+
+                // ! justification on search id 
+                await connection.query(
+                    query.sqlUpdate(`
+                        UPDATE products SET 
+                        ? 
+                        WHERE id = ?
+                    `), 
+                    [[product]], 
+                    async function(err, rows) {
+                        // * handle failed query 
+                        if(err) {
+                            // * rollback
+                            await connection.rollback(function(err) {
+                                console.error(`Query failed : ${err}`);
+                            });
+
+                            //! error handling (temporary) 
+                            return res.json({
+                                error: 1,
+                                message: err.sqlMessage,
+                            });
+                        }
+                });
+
+                // ! select data to find out the data entered by the user (temporarily) 
+                await connection.query(
+                    query.sqlSelect("SELECT * FROM products ORDER BY update_at DESC LIMIT 1"),  
+                    async function(err, rows) {
+                        // * handle failed query 
+                        if(err) {
+                            // * rollback
+                            await connection.rollback(function(err) {
+                                console.error(`Query failed: ${err}`);
+                            });
+                        } 
+                        return res.json(rows);
+                });
+            });
+
+            src.on("error", async (err) => {
+                next(err);
+            });
+        } else {
+            // * update data
+
+            // ! justification on search id 
+            await connection.query(
+                query.sqlUpdate(`
+                    UPDATE products SET
+                    ?
+                    WHERE id = ?
+                `), 
+                [[dataPayload]], 
+                async function(err, rows) {
+                    // * handle failed query 
+                    if(err) {
+                        // * rollback
+                        await connection.rollback(function(err) {
+                            console.error(`Query failed: ${err}`);
+                        });
+
+                        //! error handling (temporary) 
+                        return res.json({
+                            error: 1,
+                            message: err.sqlMessage,
+                        });
+                    }
+            });
+
+            // ! select data to find out the data entered by the user (temporarily) 
+            await connection.query(
+                query.sqlSelect("SELECT * FROM products ORDER BY update_at DESC LIMIT 1"),  
+                async function(err, rows) {
+                    // * tangani gagal query
+                    if(err) {
+                        // * rollback
+                        await connection.rollback(function(err) {
+                            console.error(`Query failed: ${err}`);
+                        });
+                    } 
+                    return res.json(rows);
+            });
+        }
+
+        // * commit
+        await connection.commit(function(err) {
+            if(err) {
+                console.error(`Failed to commit : ${err}`);
+                return;
+            }
+        });
+    }catch (err) {
+        /**
+        * ! The handler can't be used
+        * ! Need a decorator!!... 
+         */
+        // * check error type 
+        if(err && err.name === 'ValidationError'){
+            return res.json({
+                error: 1,
+                message: err.message,
+                fields: err.errors
+            });
+        }
+
+        // * error handling by express 
+        next(err);
+    }
+}
+
+module.exports = { store, index, update };
