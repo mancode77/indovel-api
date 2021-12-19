d0c238e8cb5618b9b9127770e233e9d9d3bfed2e
Aji Maulana <mancode700@gmail.com>
2021-12-19 09:33:20 +0700

    membuat schema

---
+++ app/product/model/schema.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Joi = require('joi');
+
+const productSchema = Joi.object({
+    name: Joi.string()
+        .min(3)
+        .max(100)
+        .required(),
+
+    description: Joi.string()
+				.max(1000),
+
+    price: Joi.number(),
+
+
+    image_url: Joi.string()
+				.max(100)
+				.required(),
+});
+
+// * function validation product
+async function validation(payload) {
+    const dataPayload = {
+        name: payload[0],
+        description: payload[1],
+        price: Number(payload[2]),
+        image_url: payload[3]
+    }
+   
+    // * check product structure
+    return await productSchema.validateAsync(dataPayload);
+}
+
+module.exports = { validation };
