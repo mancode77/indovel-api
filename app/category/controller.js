54244f5657621292a44d16c04d63ada4fca41f06
Aji Maulana <mancode700@gmail.com>
2021-12-21 12:53:22 +0700

    menambah fungsi hapus data

--- app/category/controller.js
+++ app/category/controller.js
@@ -11,14 +11,14 @@ async function store (req, res, next) {
         // ! START TRANSACTION
 
         // * check schema
-        const product = await categorySchema.validateAsync(payload);
+        const category = await categorySchema.validateAsync(payload);
         // * konversion object to array
-        const validProduct = Object.keys(product).map((_) => product[_]);
+        const validCategory = Object.keys(category).map((_) => category[_]);
 
         // * insert category
         await dbConnection.query( 
             `INSERT INTO categories (name)VALUES ?`,
-            [[validProduct]], 
+            [[validCategory]], 
             async function(err) {
             // * handle failed query 
                 if(err) {
@@ -67,7 +67,7 @@ async function update (req, res, next) {
 
         // ! START TRANSACTION
 
-        await queries.connectionQuery(
+        await dbConnection.query(
             "SELECT * FROM categories WHERE id = ? ",
             Number(req.params.id),
             async function(err, rows) {
@@ -83,17 +83,17 @@ async function update (req, res, next) {
         });
 
         // * check schema
-        const product = await categorySchema.validateAsync(payload);
+        const category = await categorySchema.validateAsync(payload);
         // * konversion object to array
-        const validProduct = Object.keys(product).map((_) => product[_]);
+        const validCategory = Object.keys(category).map((_) => category[_]);
 
         // * catch id user
-        validProduct.push(Number(req.params.id));
+        validCategory.push(Number(req.params.id));
 
         // * insert category
         await dbConnection.query( 
             `UPDATE categories SET name = ? WHERE id = ?`,
-            validProduct, 
+            validCategory, 
             async function(err) {
             // * handle failed query 
                 if(err) {
@@ -136,4 +136,52 @@ async function update (req, res, next) {
     }
 }
 
-module.exports = { store, update };
+async function destroy(req, res, next) {
+    try {
+       // * start transaction
+       // ! EXPERIMENTAL
+
+       // ! select data to find out the data entered by the user (temporarily) 
+       await dbConnection.query(
+           //! Binding limit and skip
+           `SELECT * FROM products WHERE id = ?`,
+           Number(req.params.id),  
+           async function(err, rows) {
+               // * handle failed query 
+               if(err) {
+                    // * ROLLBACK
+                    // ! EXPERIMENTAL
+                    console.error(err);
+
+                   return res.json({
+                       error: 1,
+                       message: err.sqlMessage,
+                   });
+               } 
+               
+              
+                await dbConnection.query(
+                       `DELETE FROM products WHERE id = ?`, 
+                       rows[0].id, 
+                       async function(){
+                           if(err) {
+                                // * ROLLBACK
+                                // ! EXPERIMENTAL
+                                console.error(err);
+
+                                return res.json({
+                                    error: 1,
+                                    message: err.sqlMessage,
+                                 });
+                           } 
+                    });
+
+               return res.json(rows);
+           });
+   }catch(err) {
+        // * error handling by express 
+       next(err);
+   }
+}
+
+module.exports = { store, update , destroy };
